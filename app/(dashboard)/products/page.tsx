dbf4647f80fa8a3e092e99b4a110f500ed9561cf
sean-canimmunize <pollen.sean@gmail.com>
2025-02-27 17:44:56 -0500

    chainsaw

--- app/(dashboard)/products/page.tsx
+++ app/(dashboard)/products/page.tsx
@@ -1,8 +1,6 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { File, PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { getProducts } from '@/lib/db';
-import { ProductsTable } from './products-table';
 
 export default async function ProductsPage(props: {
   searchParams: Promise<{ q: string; offset: string }>;
@@ -10,10 +8,6 @@ export default async function ProductsPage(props: {
   const searchParams = await props.searchParams;
   const search = searchParams.q ?? '';
   const offset = searchParams.offset ?? 0;
-  const { products, newOffset, totalProducts } = await getProducts(
-    search,
-    Number(offset)
-  );
 
   return (
     <Tabs defaultValue="all">
@@ -41,13 +35,6 @@ export default async function ProductsPage(props: {
           </Button>
         </div>
       </div>
-      <TabsContent value="all">
-        <ProductsTable
-          products={products}
-          offset={newOffset ?? 0}
-          totalProducts={totalProducts}
-        />
-      </TabsContent>
     </Tabs>
   );
 }
