1562c749f60f0218113f9806b79cd5278211e2b2
sean-canimmunize <pollen.sean@gmail.com>
2025-02-27 18:57:48 -0500

    more

---
+++ db/schema.ts
@@ -0,0 +1,10 @@
+import { text, timestamp, serial, pgTable } from 'drizzle-orm/pg-core';
+
+export const User = pgTable('users', {
+  id: serial('id'),
+  name: text('name'),
+  email: text('email'),
+  createdAt: timestamp('created_at')
+});
+
+export type User = typeof User;
