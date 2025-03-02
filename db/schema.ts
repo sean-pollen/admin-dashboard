79f0186fb762c62e6869be405819e571b40aa892
sean-canimmunize <pollen.sean@gmail.com>
2025-03-01 20:32:33 -0500

    WIP

--- db/schema.ts
+++ db/schema.ts
@@ -1,11 +1,40 @@
-import { text, timestamp, serial, pgTable } from 'drizzle-orm/pg-core';
+import { db } from 'db';
+import { eq, sql } from 'drizzle-orm';
+import { text, timestamp, serial, pgTable, uuid } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
-  id: serial('id'),
-  name: text('name'),
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
+  providerId: text('provider_id'),
+  createdAt: timestamp('created_at').notNull(),
   email: text('email'),
-  createdAt: timestamp('created_at')
+  lastLogin: timestamp('last_login')
 });
+
+export const handleLogin = async (providerId: string, email: string) => {
+  const existingUser = await db
+    .selectDistinct()
+    .from(users)
+    .where(eq(users.providerId, `${providerId}`));
+
+  // Handle create new user
+  if (existingUser.length === 0) {
+    await db.insert(users).values({
+      providerId: providerId,
+      email: email,
+      createdAt: new Date()
+    });
+  } else {
+    // Update user profile
+    await db
+      .update(users)
+      .set({
+        lastLogin: new Date()
+      })
+      .where(eq(users.providerId, `${providerId}`));
+  }
+};
 
 export type User = typeof users.$inferSelect;
 export type UserInsert = typeof users.$inferInsert;
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,12 @@
-import { asc } from 'drizzle-orm';
 import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
+import { handleLogin } from 'db/schema';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
       authorization: {
         url: 'https://accounts.spotify.com/authorize',
         params: {
@@ -17,20 +17,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     })
   ],
   callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
+    async jwt({ token, account, profile }) {
+      if (account && profile?.email) {
+        await handleLogin(account.providerAccountId, profile.email);
         token.accessToken = account.access_token;
-        token.exp = (Math.floor(Date.now() / 1000) +
-          (account?.expires_in ?? 0)) as number;
       }
       return token;
     },
+
     async session({ session, token }) {
       session.accessToken = token.accessToken as string;
-      if (typeof token.exp === 'number') {
-        // @ts-ignore
-        session.expires = new Date(Date.now() + token.exp * 1000).toISOString();
-      }
       return session;
     }
   }
